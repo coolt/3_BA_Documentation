2e1b27469697cced9c9d38fceaeb728fe9d42266
Balingor <manuel.koenig@hotmail.com>
2016-03-18 17:58:30 +0100

    Messung Energiekurve von neuem Aufbau angefangen

---
+++ Messung_Energiemessung_Harvester2/CSV_Verwertung.m
@@ -0,0 +1,25 @@
+clc, clear all;
+
+%% Vor dem Ausführen folgende Variabeln definiren
+
+filename = 'Harvester2_40Ohm.csv'; % evtl. Dateiname anpassen
+R = 40; % verwendeten Widerstand eintragen
+v = 10; % eingestellte Geschwindigkeit eintragen
+
+%% Laden der Daten aus dem CSV-File
+
+data = csvread(filename, 16,1);
+data_points = csvread(filename, 8, 1, [8, 1, 8, 1]);
+x_resolution = csvread(filename, 5, 1, [5, 1, 5, 1]);
+
+%% Variabeln berechnen
+
+resolution = x_resolution * 10 / data_points;
+period = 1/(v * 1000 / 3600 / 2.04);
+data_range = period / resolution;
+
+%% Durchschnittliche Spannung, Strom, Leistung berechnen
+
+u_mean = (sum(data(1:data_range)))/(data_range+1)
+i_mean = u_mean / R
+p_mean = i_mean * u_mean
