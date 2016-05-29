b305d4bf471d3eb12c03bf214089f963d6c28662
Balingor <manuel.koenig@hotmail.com>
2016-05-29 14:56:01 +0200

    Bild überarbeitet

--- Texte von Manuel/BILD_MPP/MPP.m
+++ Texte von Manuel/BILD_MPP/MPP.m
@@ -2,8 +2,8 @@ clc, clear all;
 
 u = linspace(0,1,1000);
 
-%% MPP 40%
-offset = 0.4;
+%% MPP 10km/h
+offset = 0.454;
 p = -(u-offset).^2;
 
 for n = 1:(1000*offset)
@@ -13,8 +13,8 @@ for n = (1000*offset+1):1000
     p1(n) = (1/p(1000))*((n/1000-offset)^2)+1;
 end
 
-%% MPP 50%
-offset = 0.5;
+%% MPP 15km/h
+offset = 0.377;
 p = -(u-offset).^2;
 
 for n = 1:(1000*offset)
@@ -24,9 +24,9 @@ for n = (1000*offset+1):1000
     p2(n) = (1/p(1000))*((n/1000-offset)^2)+1;
 end
 
-%% MPP 60%
+%% MPP 20km/h
 
-offset = 0.6;
+offset = 0.541;
 p = -(u-offset).^2;
 
 for n = 1:(1000*offset)
@@ -36,9 +36,9 @@ for n = (1000*offset+1):1000
     p3(n) = (1/p(1000))*((n/1000-offset)^2)+1;
 end
 
-%% MPP 70%
+%% MPP 40km/h
 
-offset = 0.7;
+offset = 0.681;
 p = -(u-offset).^2;
 
 for n = 1:(1000*offset)
@@ -52,7 +52,7 @@ end
 plot(u,p1,u,p2,u,p3,u,p4)
 axis([0 1 0 1])
 grid minor
-legend('MPP 1','MPP 2', 'MPP 3', 'MPP 4')
+legend('10km/h','15km/h', '20km/h', '40km/h')
 xlabel('Spannung (normalisiert)')
 ylabel('Leistung (normalisiert)')
 title('beispielhafte Entwicklung MPP')
